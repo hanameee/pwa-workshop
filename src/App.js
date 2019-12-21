02e168c21741b9b3d2482f14ec1318937f770f2b
hanameee <hanameee@likelion.org>
2019-12-21 17:25:45 +0900

    Update workflow

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-const Write = (props) => {
-  const {addData} = props;
-  const [content, setContent] = useState('');
+const Write = props => {
+    const { addData } = props;
+    const [content, setContent] = useState("");
 
-  const handleWriting = (e) => {
-    setContent(e.target.value);
-  }
+    const handleWriting = e => {
+        setContent(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addData(content);
-    setContent('');
-  }
+    const handleSubmit = e => {
+        e.preventDefault();
+        addData(content);
+        setContent("");
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <textarea onChange={handleWriting} value={content} />
-      <button>전송하기</button>
-    </form>
-  )
-}
+    return (
+        <form onSubmit={handleSubmit}>
+            <textarea onChange={handleWriting} value={content} />
+            <button>전송하기</button>
+        </form>
+    );
+};
 
-const List = (props) => {
-  const { data } = props;
+const List = props => {
+    const { data } = props;
 
-  return (
-    <div className="list">
-      {data.map(item => (
-        <div key={item.id}>{item.value}</div>
-      ))}
-    </div>
-  )
-}
+    return (
+        <div className="list">
+            {data.map(item => (
+                <div key={item.id}>{item.value}</div>
+            ))}
+        </div>
+    );
+};
 
 const App = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  const addData = (content) => {
-    const id = data.length + 1;
+    const addData = content => {
+        const id = data.length + 1;
 
-    setData([
-      ...data,
-      {
-        id: id,
-        value: content
-      }
-    ])
-  }
+        setData([
+            ...data,
+            {
+                id: id,
+                value: content
+            }
+        ]);
+    };
 
-  return (
-    <div className="notes">
-      <Write addData={addData} />
-      <h1>List:</h1>
-      <List data={data} />
-    </div>
-  )
-}
+    return (
+        <div className="notes">
+            <h1>
+                정-하<span role="img">😀</span>
+            </h1>
+            <Write addData={addData} />
+            <h2>List:</h2>
+            <List data={data} />
+        </div>
+    );
+};
 
 export default App;
